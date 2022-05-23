c30df217b06f6cb52f9658168faa4f367871a3db
Michał Janiszewski <janisozaur+signed@gmail.com>
2022-05-23 15:48:27 +0200

    Make sure it compiles without errors

--- script.sh
+++ script.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -e
-c++ -Og -std=gnu++17  -o temp.o -c temp.cpp -Werror=maybe-uninitialized
+c++ -Og -std=gnu++17  -o temp.o -c temp.cpp -Werror=maybe-uninitialized -Werror
 c++ -O2 -std=gnu++17  -o temp.o -c temp.cpp -Werror=maybe-uninitialized 2>&1 | grep Werror=maybe-uninitialized
