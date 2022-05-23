d9258021ff162c26fe29ecefcf1dbf80ad8c4b3e
Michał Janiszewski <janisozaur+signed@gmail.com>
2022-05-23 16:50:12 +0200

    Allow optionally passing filename

--- script.sh
+++ script.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 set -e
-c++ -Og -std=gnu++17  -o temp.o -c temp.cpp -Werror=maybe-uninitialized -Werror
-c++ -O2 -std=gnu++17  -o temp.o -c temp.cpp -Werror=maybe-uninitialized 2>&1 | egrep "offsetof.*std::optional.*std::optional.*Werror=maybe-uninitialized"
+DEFAULTVALUE="temp.cpp"
+FILE="${1:-$DEFAULTVALUE}"
+c++ -Og -std=gnu++17  -o temp.o -c $FILE -Werror=maybe-uninitialized -Werror
+c++ -O2 -std=gnu++17  -o temp.o -c $FILE -Werror=maybe-uninitialized 2>&1 | egrep "offsetof.*std::optional.*std::optional.*Werror=maybe-uninitialized"
